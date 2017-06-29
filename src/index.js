42b3308054905db27a5a45a6834be58ed1428588
Jean-Luc Canela <jlcanelam@gmail.com>
2017-06-29 12:11:55 +0200

    adding service-checker endpoint

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('b3ec-ca19-2340-90ac');
 });
 
+app.get('/admin/services-checker', function (req, res) {
+  res.send({});
+});
+
+
 app.listen(1337, function () {
   console.log('Example app listening on port 1337!');
 });
