40780b835167beaa9c3449d450003c3f46751e6d
Jean-Luc Canela <jeanluc.canela@axa-groupsolutions.com>
2017-06-29 10:09:01 +0200

    first revision

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('b3ec-ca19-2340-90ac');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
